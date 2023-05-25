366149135c974ea695ea694d57b28f992c2b9f78
Braden Clevenger <bradenclevenger5@icloud.com>
2023-05-25 19:48:16 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const svgWrite = require('svgWrite');
-const readline = require('readline');
-const fs = require('fs');
+import {SVG} from '@svgdotjs/svgjs';
+import { createInterface } from 'readline';
+import { writeFileSync } from 'fs';
 
-const rl = readline.createInterface({
+const rl = createInterface({
     input: process.stdin,
     output: process.stdout
 });
@@ -25,22 +25,29 @@ rl.question('Enter up tp three characters: ', (text) => {
                 svg.circle({ cx: '150', cy: '100', r:'50', fill: shapeColor});
                 break;
                 case 'triangle':
-                    svg.polygon([(150 - shapeSize.width / 2) + ',' + (100 + shapeSize.height / 2)]),
-                    (150 - shapeSize.width / 2) + ',' + (100 + shapeSize.height / 2),
-                    ('150' + (100 - shapeSize.height / 2), {fill: shapeColor});
+                    svg.polygon([
+                        '${150 - shapeSize.width / 2},${100 + shape.height / 2}',
+                        '${150 - shapeSize.width / 2},${100 + shape.height / 2}',
+                        '${150 - shapeSize.width / 2},${100 + shape.height / 2}'
+                    ], { fill: shapeColor});
             break;
         case 'square':
-            svg.rect((150 - shapeSize.width / 2) + ',' + (100 - shapeSize.height / 2)),
-                   shapeSize.width, shapeSize.height, { fill: shapeColor};
+            svg.rect(
+                '${150 - shapeSize.width / 2}${100 + shapeSize.height / 2}',
+                   shapeSize.width, 
+                   shapeSize.height, 
+                   { fill: shapeColor}
+            );
             break;
             default:
                 console.log('Invalid shape entered');
+                rl.close();
                 return;
 
                 svg.text(text, { x: '150', y:'100', fill: textColor, 'font-size': textSize, 'text-anchor': 'middle'});
 
                 const filename = 'logo.svg';
-                fs.writeFileSync(filename, svg.toString());
+                writeFileSync(filename, svg.toString());
                 console.log('Generated ${filename}');
 
                 const browserPath = process.env.Browser || '/usr/bin/xdg-open';
